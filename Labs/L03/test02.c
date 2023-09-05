7214acf6d92e4fcd5b843f38c6efd101c573b935
hamza26051 <142868640+hamza26051@users.noreply.github.com>
2023-09-05 10:08:56 +0500

    Add files via upload

---
+++ Labs/L03/test02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int height=20;
+	int length=10;
+	int width=10;
+                 		printf(" the volume of the container is %d" , height*length*width);
+
+	return 0;
+}
