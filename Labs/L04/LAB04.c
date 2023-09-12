9ba6bb71e8b800e48e42a6af39f056927d6a2260
hamza26051 <142868640+hamza26051@users.noreply.github.com>
2023-09-12 10:00:53 +0500

    Add files via upload

---
+++ Labs/L04/LAB04.c
@@ -0,0 +1,28 @@
+/*
+* Programmer: Hamza Sheikh
+* Date: 12/09/23
+* Description: print the pattern for any number of n
+*/
+
+//--Include Files--//
+#include <stdio.h>
+
+int main(){
+int n;
+
+char ch= '*';
+printf("enter a number n");
+scanf("%d", &n);
+int i=0:
+do  {
+i+=
+} while (i<n)
+int z=0
+do {
+z++
+printf("*");
+} while (z<=i)
+printf("\n");
+}
+return 0;
+}
